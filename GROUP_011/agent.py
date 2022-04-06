08d45e5853f906ec7f0c90491ead43f251e85481
Dragos Cristian Manta <dragos.manta@yahoo.ca>
2022-04-05 23:03:23 -0400

    Implemented less sparse feature encoding, but the performance is exactly the same as with the sparse initial features.

--- GROUP_011/agent.py
+++ GROUP_011/agent.py
@@ -13,21 +13,45 @@ class Agent():
     self.gamma = 0.99
     self.eps = 0.1
     self.num_actions = 4
-    self.input_size = 1582
+    self.input_size = 15 * 15
     self.w = np.random.randn(self.input_size)
+
+    self.action_position_embeds = [(7, 8), (6, 7), (8, 7), (7, 6)]
 
   def load_weights(self, root_path):
     # Add root_path in front of the path of the saved network parameters
     # For example if you have weights.pth in the GROUP_MJ1, do `root_path+"weights.pth"` while loading the parameters
     pass
-
+  
   def encode_features(self, curr_obs, a):
-    feats = np.concatenate((curr_obs[0].flatten(), curr_obs[1].flatten(), curr_obs[2].flatten()))
-    action = np.zeros(self.num_actions)
-    action[a] = 1
-    feats = np.concatenate((feats, action))
+    """
+    In curr_obs[2].reshape((15, 15, 4)), the last dimension encodes the following:
+    Jelly bean -> [0, 0, 1, 0]
+    Truffle -> [0, 0, 0, 1]
+    Banana -> [0, 1, 0, 0]
+    Apple -> [1, 0, 0, 0]
 
-    return feats
+    In our encoding, we give them, respectively, the values below:
+    """    
+    jb_val = 1
+    truffle_val = 2
+    banana_val = 3
+    apple_val = 4
+    empty_val = 0 # This might be too naive
+
+    feats = curr_obs[2].reshape((15, 15, 4))
+    feats = np.concatenate((feats, np.zeros((15, 15, 1), dtype=int)), axis=2) # Need to add fifth indicator with a "1" iff there's no object there
+    for i in range(15):
+      for j in range(15):
+        if sum(feats[i][j]) == 0:
+          feats[i][j][-1] = 1    
+
+    feats = feats.astype(bool)
+    values = np.broadcast_to(np.array([apple_val, banana_val, jb_val, truffle_val, empty_val]), (15, 15, 5))
+    feats = values[feats].reshape((15, 15))
+    feats[self.action_position_embeds[a][0], self.action_position_embeds[a][1]] += 1 # Adding the action embedding
+
+    return feats.flatten()
 
   def act(self, curr_obs, mode='eval'):
     if mode == 'train':
