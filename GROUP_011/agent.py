d4a5fa9e641af0a4458c2285ef32dd03ec338f99
Dragos Cristian Manta <dragos.manta@yahoo.ca>
2022-04-12 13:04:12 -0400

    Changed path string

--- GROUP_011/agent.py
+++ GROUP_011/agent.py
@@ -66,13 +66,13 @@ class Agent():
     self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.alpha, momentum=0.9, weight_decay=5e-4)
     self.criterion = nn.MSELoss()
 
-  def load_weights(self, path=osp.join("GROUP_011", "weights.pth")):
+  def load_weights(self, path=osp.join("root_path", "weights.pth")):
     # Add root_path in front of the path of the saved network parameters
     # For example if you have weights.pth in the GROUP_MJ1, do `root_path+"weights.pth"` while loading the parameters
     self.model.load_state_dict(torch.load(path))
     
 
-  def save_weights(self, path=osp.join("GROUP_011", "weights.pth")):
+  def save_weights(self, path=osp.join("root_path", "weights.pth")):
     torch.save(self.model.state_dict(), path)
 
   
