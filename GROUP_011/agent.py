53938d2226ea469670930a0ca7ddff0180f3c669
KC Tsiolis <kc.tsiolis@mail.mcgill.ca>
2022-04-13 23:01:42 -0400

    Adding DQN to the mix

--- GROUP_011/agent.py
+++ GROUP_011/agent.py
@@ -2,10 +2,10 @@ import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
-import os
 import os.path as osp
+from copy import deepcopy
 
-class LeNet(nn.Module):
+class DQN(nn.Module):
   def __init__(self):
     super().__init__()
     self.conv1 = nn.Conv2d(in_channels=4, out_channels=6, kernel_size=3)
@@ -22,13 +22,38 @@ class LeNet(nn.Module):
     x = self.fc2(x)
     return torch.flatten(x)
 
-class LinearModel(nn.Module):
-  def __init__(self, input_size):
-    super().__init__()
-    self.w = nn.Linear(in_features=input_size, out_features=4)
+#Inspired by PyTorch tutorial: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+class ReplayBuffer(object):
 
-  def forward(self, x):
-    return torch.flatten(self.w(x))
+    def __init__(self, capacity):
+        self.buffer = []
+        self.size = 0
+        self.capacity = capacity
+
+    def add(self, curr_obs, action, reward, next_obs):
+        """Save a transition"""
+        self.buffer.append([curr_obs, action, reward, next_obs])
+        if self.size == self.capacity:
+            self.buffer.pop(0)
+        else:
+            self.size += 1
+
+    def sample(self, batch_size):
+        sample = []
+        for _ in range(batch_size):
+          index = np.random.randint(low=0, high=self.size)
+          sample.append(self.buffer[index])
+        return sample
+
+    def __len__(self):
+        return self.size
+
+def make_target_model(model):
+    target_model = deepcopy(model)
+    for param in target_model.parameters():
+        param.requires_grad = False
+
+    return target_model
 
 class Agent():
   '''The agent class that is to be filled.
@@ -36,35 +61,30 @@ class Agent():
      want to this class.
   '''
 
-  def __init__(self, env_specs, model_str='lenet', encoding_method='grid', do_save_weights=True, save_freq=4999, pretrained=False):
+  def __init__(self, env_specs, do_save_weights=True, save_freq=4999, pretrained=False):
     self.env_specs = env_specs
-    self.alpha = 0.001
-    self.gamma = 0.9
-    self.eps = 0.25
+    self.encode_features = self.encode_features_grid
+    self.lr = 0.001
+    self.gamma = 0.99
+    self.eps = 1
+    self.final_eps = 0.1
+    self.eps_anneal_steps = 1e+5 #Timespan over which to decay epsilon
+    self.buffer_capacity = 10000
+    self.buffer = ReplayBuffer(self.buffer_capacity)
+    self.target_update_freq = 5000
+    self.batch_size = 32
     self.num_actions = 4
-
-    if encoding_method == 'dense':      
-      self.input_size = 15 * 15      
-      self.encode_features = self.encode_features_dense
-      self.action_position_embeds = [(7, 8), (6, 7), (8, 7), (7, 6)]
-    elif encoding_method == 'sparse':      
-      self.input_size = 900      
-      self.encode_features = self.encode_features_sparse
-    elif encoding_method == 'grid':
-      self.encode_features = self.encode_features_grid
 
     self.do_save_weights = do_save_weights
     self.save_freq = save_freq
-    if model_str == 'lenet':
-      self.model = LeNet()
-    elif model_str == 'linear':
-      self.model = LinearModel(self.input_size)
 
+    self.model = DQN()
+    self.target_model = make_target_model(self.model)
     if pretrained:
       self.load_weights()
     self.model.train()
 
-    self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.alpha, momentum=0.9, weight_decay=5e-4)
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
     self.criterion = nn.MSELoss()
 
   def load_weights(self, root_path="./"):
@@ -78,42 +98,6 @@ class Agent():
   def save_weights(self, root_path="./"):
     full_path = root_path + "weights.pth"
     torch.save(self.model.state_dict(), full_path)
-
-  
-  def encode_features_dense(self, curr_obs):
-    """
-    In curr_obs[2].reshape((15, 15, 4)), the last dimension encodes the following:
-    Jelly bean -> [0, 0, 1, 0]
-    Truffle -> [0, 0, 0, 1]
-    Banana -> [0, 1, 0, 0]
-    Apple -> [1, 0, 0, 0]
-
-    In our encoding, we give them, respectively, the values below:
-    """    
-    jb_val = -2
-    truffle_val = -1.1
-    banana_val = 1
-    apple_val = 2
-    empty_val = 0 # This might be too naive
-
-    feats = curr_obs[2].reshape((15, 15, 4))
-    feats = np.concatenate((feats, np.zeros((15, 15, 1), dtype=int)), axis=2) # Need to add fifth indicator with a "1" iff there's no object there
-    for i in range(15): # If you see a more compact (and fast) way to do this operation, please modify it or let me know
-      for j in range(15):
-        if sum(feats[i][j]) == 0:
-          feats[i][j][-1] = 1    
-
-    feats = feats.astype(bool)
-    values = np.broadcast_to(np.array([apple_val, banana_val, jb_val, truffle_val, empty_val]), (15, 15, 5))
-    feats = values[feats].reshape((15, 15))
-    feats = torch.from_numpy(feats.flatten())
-    return feats.float()
-
-  def encode_features_sparse(self, curr_obs):
-    feats = torch.from_numpy(curr_obs[2]).flatten()
-    #feats = torch.concatenate((curr_obs[0].flatten(), curr_obs[1].flatten(), curr_obs[2].flatten()))
-
-    return feats.float()
 
   def encode_features_grid(self, curr_obs):
     curr_obs = torch.from_numpy(curr_obs[2])
@@ -132,18 +116,41 @@ class Agent():
 
   def update(self, curr_obs, action, reward, next_obs, done, timestep):
     self.optimizer.zero_grad()
-    curr_feats = self.encode_features(curr_obs)
-    cur_q = self.model(curr_feats)[action]
     if done:
-      reward = torch.as_tensor(reward)
-      loss = self.criterion(cur_q, reward)
-    else:
-      next_feats = self.encode_features(next_obs)
-      next_action = self.act(next_obs, mode='train')
-      next_q = self.model(next_feats)[next_action]
-      loss = self.criterion(cur_q, reward + self.gamma * next_q)
+        next_obs = None
+    #Add observation to replay buffer
+    self.buffer.add(curr_obs, action, reward, next_obs)
 
+    if (timestep % self.target_update_freq) == 0:
+        #Update target network
+        self.target_model = make_target_model(self.model)
+
+    if timestep < self.buffer_capacity:
+        #No learning until buffer is full
+        return
+    elif timestep < self.eps_anneal_steps:
+        #Annealing epsilon
+        self.eps = 0.9/(self.eps_anneal_steps - self.buffer_capacity) * timestep
+
+    #Sample a batch
+    batch = self.buffer.sample(self.batch_size)
+    estimates = torch.zeros(self.batch_size)
+    targets = torch.zeros(self.batch_size)
+    for i, (curr_obs, action, reward, next_obs) in enumerate(batch):
+        curr_feats = self.encode_features(curr_obs)
+        cur_q = self.model(curr_feats)[action]
+        estimates[i] = cur_q
+        if next_obs is None:
+            targets[i] = torch.as_tensor(reward)
+        else:
+            next_feats = self.encode_features(next_obs)
+            next_action = self.act(next_obs, mode='eval')
+            next_q = self.target_model(next_feats)[next_action]
+            targets[i] = reward + self.gamma * next_q
+
+    loss = self.criterion(estimates, targets)
     loss.backward()
     self.optimizer.step()
+
     if (timestep % self.save_freq == 0) and self.do_save_weights:
       self.save_weights()
